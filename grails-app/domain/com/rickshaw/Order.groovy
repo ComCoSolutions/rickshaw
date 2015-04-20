105171dbc0614545be2cec8bc9807a6b15453ffc
Moe B <moeb59@gmail.com>
2015-04-20 11:12:51 -0400

    Implemented order domain class.

--- grails-app/domain/com/rickshaw/Order.groovy
+++ grails-app/domain/com/rickshaw/Order.groovy
@@ -1,7 +1,16 @@
+/*
+    This class will represent order objects in the rickshaw application. Specifically, an order object
+    represents and order placed by a customer.
+*/
 package com.rickshaw
 
 class Order {
+    Date dateCreated
+    Float total
 
+    static belongsTo = [customer: Customer]
+    //static hasMany = [orderItems: OrderItem]
     static constraints = {
+        total nullable: false
     }
 }
