07eb0e10e1f87b6ad0a52aebd336c0040e9af9f6
Moe B <moeb59@gmail.com>
2015-04-22 11:13:42 -0400

    First integration test isn't running!

--- test/integration/com/rickshaw/CustomerIntegrationSpec.groovy
+++ test/integration/com/rickshaw/CustomerIntegrationSpec.groovy
@@ -8,13 +8,16 @@ import spock.lang.*
  *
  */
 class CustomerIntegrationSpec extends Specification {
+    def "Saving our first customer to the database" () {
+        given: "A brand new customer"
+            def john = new Customer(firstName: 'John', lastName: 'Doe',
+                                        email: 'jdoe@hotmail.com', password: 'secret')
+        when: "the customer is saved"
+            john.save()
+        then: "it saved successfully and can be found in the database"
+            john.errors.errorCount == 0
+            john.id != null
+            Customer.get(john.id).firstName == 'John'
 
-    def setup() {
-    }
-
-    def cleanup() {
-    }
-
-    void "test something"() {
     }
 }
